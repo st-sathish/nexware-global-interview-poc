9ff58677ed1c14beaa77e59284560f54995ff258
Sathish <sathish.2009btech@gmail.com>
2020-07-02 23:42:27 +0530

    fix code cleanup

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 ### Note
+
 - Install OpenJDK, MySQL, Maven services
 ```sh
 mvn clean install
 ```
+
+- Screenshots are available inside docs/screenshot folder
--- src/main/java/com/nextgenglobal/palindrome/form/PalindromeFormValidator.java
+++ src/main/java/com/nextgenglobal/palindrome/form/PalindromeFormValidator.java
@@ -9,8 +9,15 @@ import com.nextgenglobal.palindrome.utils.MathUtils;
 public class PalindromeFormValidator {
 
 	public void validateForm(final PalindromeForm form) {
+		String name = form.getName();
 		if(form.getName() == null || form.getName().equals("")) {
 			throw new RuntimeException("Name shouldn't be empty");
+		}
+		if(name != null && !name.matches("^[a-zA-Z]*$")) {
+			throw new RuntimeException("Name should allow only Alphabets");
+		}
+		if(form.getName().trim().length() > 100) {
+			throw new RuntimeException("Name should be a range 0 - 100");
 		}
 		if(!MathUtils.isPalindrome(form.getName())) {
 			throw new InvalidPalindromeException("Palindrome not valid");
